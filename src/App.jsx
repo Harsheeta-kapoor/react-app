a6a791d721a631aa0e6d3a3de2d33b044026460d
Harsheeta <harsheeta.kap@gmail.com>
2023-08-26 19:05:47 +0530

    first commit of react app

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+
+// const Conponent = () => {
+//   return<h4>i am a component </h4>
+//   }
+
+// import mybutton from "./components/myButton";
+
+// const App = () => {
+//   return
+//   <div>
+//     <h1>component</h1>
+//     <title heading="my first react app" />
+//     <button title="button one" text="click me" />
+//   </div>
+// }
+
+// export default App
+
+// src/App.jsx
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
+
